0af9d2323eae77b88e039deeb62ebfe3bb62f779
zeinali <zeinali.b69@yahoo.com>
2022-02-23 09:41:32 +0330

    layout initial changes

---
+++ components/MovieHero.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hero">
+    <v-carousel :show-arrows="false" cycle height="400">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieHero',
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+</style>
