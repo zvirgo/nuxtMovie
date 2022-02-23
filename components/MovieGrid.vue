969999d01efcebdd5258a68232eec4c694aa344c
zeinali <zeinali.b69@yahoo.com>
2022-02-23 14:23:50 +0330

    design movie grid

---
+++ components/MovieGrid.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="movie-grid">
+    <v-container>
+      <v-row>
+        <div v-for="(movie, index) in movies" :key="index" class="movie px-2">
+          <v-card :loading="loading" class="mx-auto my-12" max-width="270">
+            <template slot="progress">
+              <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+
+            <v-img
+              height="350"
+              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+            ></v-img>
+
+            <v-card-title
+              >{{ movie.title.slice(0, 20) }}
+              <span v-if="movie.title.length > 20">...</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-row align="center" class="mx-0">
+                <v-rating
+                  :value="movie.vote_average"
+                  length="10"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">{{ movie.vote_average }}</div>
+              </v-row>
+
+              <div class="my-4 text-subtitle-2">
+                {{
+                  new Date(movie.release_date).toLocaleString('en-us', {
+                    month: 'long',
+                    day: 'numeric',
+                    year: 'numeric',
+                  })
+                }}
+              </div>
+
+              <div>
+                {{ movie.overview.slice(0, 90) }}
+                <span v-if="movie.overview.length > 90">...</span>
+              </div>
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-actions>
+              <nuxt-link
+                :to="{
+                  name: 'movies-movieid',
+                  params: { movieid: movie.id },
+                }"
+                class="infoBtn"
+              >
+                Get More Info</nuxt-link
+              >
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieGrid',
+  props: {
+    movies: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      loading: false,
+      selection: 1,
+    }
+  },
+  computed: {},
+  methods: {
+    reserve() {
+      this.loading = true
+
+      setTimeout(() => (this.loading = false), 2000)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.infoBtn {
+  align-items: center;
+  color: inherit;
+  display: flex;
+  flex: 1 0 auto;
+  justify-content: inherit;
+  line-height: normal;
+  position: relative;
+  transition: inherit;
+  transition-property: opacity;
+  color: #9575cd !important;
+  text-decoration: none;
+  padding: 0 8px;
+  height: 36px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="home">
+    <!--Hero -->
     <movie-hero />
+
+    <!--Movies -->
+    <movie-grid :movies="movies" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import MovieHero from '../components/MovieHero.vue'
+import MovieGrid from '../components/MovieGrid.vue'
 export default {
   name: 'IndexPage',
   components: {
     MovieHero,
+    MovieGrid,
+  },
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=e713fd0a545ced6d78a33f57811ee90a&language=en-US&page=1`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+      console.log(this.movies)
+    },
   },
 }
 </script>
